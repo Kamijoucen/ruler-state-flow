4146817f4110a84030591e940e67784c765b9af1
lisicen <lisc@dustess.com>
2022-01-21 15:19:22 +0800

    init

---
+++ src/main/java/com/kamijoucen/stateflow/Main.java
@@ -0,0 +1,9 @@
+package com.kamijoucen.stateflow;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
---
+++ src/test/java/com/kamijoucen/stateflow/test/DemoTest.java
@@ -0,0 +1,12 @@
+package com.kamijoucen.stateflow.test;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @Test
+    public void test1() {
+        System.out.println("test");
+    }
+
+}
